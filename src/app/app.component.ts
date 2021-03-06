64c5b15449072097a101e2b3d9dbb0b433168fe2
pixelschubs <fwest@posteo.de>
2021-03-06 15:53:20 +0200

    added csv loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgxCsvParser } from 'ngx-csv-parser';
+import { HttpClient} from "@angular/common/http"
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private ngxCsvParser: NgxCsvParser, private http: HttpClient) {
+    this.http.get('data/one.txt', { responseType: 'text'})
+    .subscribe(data => {
+      console.log(data);
+    });
+  }
+
   title = 'mein erstes chart';
 
   config : zingchart.graphset = {
-    type: 'line', 
-      series: [{ values: [3,4,5,5,6,7,5,3] }] 
+    title: {text: 'jee'},
+    type: 'bar', 
+    series: [{ values: [3.5,4,5,5,6,7,5,3,1,2,3] }],
+    "scale-x": {
+        // Set scale label
+        label: { text: 'Months' },
+        // Convert text on scale indices
+        
+        values: [ 'January', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'October', 'November', 'December']
+    }
   };
 }
