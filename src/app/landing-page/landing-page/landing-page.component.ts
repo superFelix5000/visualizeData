e750466364964052840db0499130c7a5bb9695e4
pixelschubs <fwest@posteo.de>
2021-04-10 15:04:48 +0300

    minor refactoring

--- src/app/landing-page/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page/landing-page.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class LandingPageComponent {
 
 }
