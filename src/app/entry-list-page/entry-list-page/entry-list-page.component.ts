14f04047bd475f816aa5d9fbd2ca4ad8e0232c17
pixelschubs <fwest@posteo.de>
2021-04-10 19:26:50 +0300

    added new component for lists

---
+++ src/app/entry-list-page/entry-list-page/entry-list-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-list-page',
+  templateUrl: './entry-list-page.component.html',
+  styleUrls: ['./entry-list-page.component.scss']
+})
+export class EntryListPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
