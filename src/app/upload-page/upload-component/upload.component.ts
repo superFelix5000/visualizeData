af7efb94b9976147c173ac7e25f21f9a9e357cfe
pixelschubs <fwest@posteo.de>
2021-12-19 00:40:08 +0200

    linted new files!

--- src/app/upload-page/upload-component/upload.component.ts
+++ src/app/upload-page/upload-component/upload.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-upload-component',
-  templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss']
+    selector: 'app-upload-component',
+    templateUrl: './upload.component.html',
+    styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent {
 
