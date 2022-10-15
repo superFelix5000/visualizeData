4ed2df7bf257b7c0d085b0d75d15f16182222e65
Felix <fwest@posteo.de>
2022-10-15 23:57:32 +0300

    implemented chart.js pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,56 +1,79 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
-import { CategoryColorMap } from 'src/app/shared/category-colors';
-import { CategoryPercentage } from 'src/app/shared/category-percentage';
-import { BankDataQuery } from 'src/app/state/bank.data.query';
-import { BankDataService } from 'src/app/state/bank.data.service';
-import zingchart from 'zingchart/es6';
+    import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+    import { filter, map, tap } from 'rxjs/operators';
+    import { CategoryColorMap } from 'src/app/shared/category-colors';
+    import { CategoryPercentage } from 'src/app/shared/category-percentage';
+    import { BankDataQuery } from 'src/app/state/bank.data.query';
+    import { BankDataService } from 'src/app/state/bank.data.service';
+    import { Chart, registerables } from 'chart.js';
 
-@Component({
-    selector: 'app-pie-chart',
-    templateUrl: './pie-chart.component.html',
-    styleUrls: ['./pie-chart.component.scss'],
-})
-export class PieChartComponent implements OnInit {
-    series$: Observable<zingchart.series[]>;
-    private categoryPercentages: CategoryPercentage[];
-    myConfig: zingchart.graphset = {
-        type: 'pie',        
-        plot: {            
-          valueBox: {
-            placement: 'out',
-            text: '%t\n%npv%',
-            fontFamily: "Roboto"
-          },
-          tooltip: {
-            fontSize: '18',
-            fontFamily: "Roboto",
-            padding: "5 10",
-            text: "%t\n%npv%\n%v",
-            decimals: 2
-          }
+    @Component({
+        selector: 'app-pie-chart',
+        templateUrl: './pie-chart.component.html',
+        styleUrls: ['./pie-chart.component.scss'],
+    })
+    export class PieChartComponent implements OnInit {
+        @ViewChild('canvas', {static: true}) myCanvas: ElementRef<HTMLCanvasElement>;
+
+        private categoryPercentages: CategoryPercentage[];           
+        private myChart: Chart;
+
+        constructor(private bankDataQuery: BankDataQuery,
+                    private bankDataService: BankDataService) {
+                    Chart.register(...registerables);
+                    }
+
+        ngOnInit(): void {
+            const labels = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+            ];
+
+            this.myChart = new Chart(this.myCanvas.nativeElement.getContext('2d'), {
+                type: 'doughnut',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        data: [],
+                        backgroundColor: [],
+                        hoverOffset: 4
+                    }]
+                },
+                options: {}
+            });
+
+            this.bankDataQuery.selectAllCategoriesPerSelectedYearAndMonth$.pipe(
+                filter(values => values.length > 0),
+                map(values => values.sort((a,b) => a.category.toString().localeCompare(b.category.toString())))
+            ).subscribe(values => {
+                var labels: string[] = [];
+                var colors: string[] = [];
+                var dataPoints: number[] = [];
+
+                values.map(categoryPercentage => {
+                        labels.push(categoryPercentage.category.toString());
+                        colors.push(CategoryColorMap.get(categoryPercentage.category));
+                        dataPoints.push(categoryPercentage.totalValue);
+                    }
+                );
+                this.myChart.data = {
+                    labels: labels,
+                    datasets: [{
+                        data: dataPoints,
+                        backgroundColor: colors,
+                        hoverOffset: 4
+                    }]
+                };
+                this.myChart.update();
+            });
         }
-      };      
 
-    constructor(private bankDataQuery: BankDataQuery,
-                private bankDataService: BankDataService) {}
-
-    ngOnInit(): void {
-        this.series$ = this.bankDataQuery.selectAllCategoriesPerSelectedYearAndMonth$.pipe(
-            map(values => values.sort((a,b) => a.category.toString().localeCompare(b.category.toString()))),
-            tap(values => this.categoryPercentages = values),
-            map(values => values.map(categoryPercentage => {
-                return {
-                    values: [categoryPercentage.totalValue],
-                    text: categoryPercentage.category.toString(),
-                    backgroundColor: CategoryColorMap.get(categoryPercentage.category)
-                }
-            })));
+        // TODO: add type for event?
+        // TODO: re-implement this
+        nodeClicked(ev) {
+            this.bankDataService.setCategory(this.categoryPercentages[ev.plotindex].category);
+        }
     }
-
-    // TODO: add type for event?
-    nodeClicked(ev) {
-        this.bankDataService.setCategory(this.categoryPercentages[ev.plotindex].category);
-    }
-}
